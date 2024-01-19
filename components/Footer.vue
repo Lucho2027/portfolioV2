6fcd924bde4972aa8294884e6db36efbf581ab37
Lucho2027 <luis01.ro@gmail.com>
2024-01-19 11:34:11 -0600

    chore: fixed styling on footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -11,25 +11,20 @@ const isDark = computed({
 });
 </script>
 <template>
-  <footer class="sticky p-2 z-[10] bottom-0 duration-200 px-6">
+  <footer class="sticky p-2 z-[10] bottom-0 duration-200 text-center px-6">
     <div class="hidden sm:flex">
-      <p class="px-4 py-2 font-medium">Connect with me</p>
-      <div class="flex flex-col gap-4 items-center justify-center">
+      <div class="flex gap-4 items-center w-full justify-center">
         <p><b class="pr-2">Email</b> luis01.ro@gmail.com</p>
         <p>
           <b class="pr-2">GitHub</b>
           <a href="https://github.com/Lucho2027" target="_blank" class=""
-            >Lucho2027<sup class=""
-              ><span class="text-xs scale-75 pl-0.5"
-                ><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" /></span></sup
+            >Lucho2027<sup class=""><span class="text-xs scale-75 pl-0.5"></span></sup
           ></a>
         </p>
         <p>
           <b class="pr-2">LinkedIn</b>
           <a href="https://www.linkedin.com/in/luisgrodriguezs" target="_blank" class=""
-            >luisgrodriguezs<sup class=""
-              ><span class="text-xs scale-75 pl-0.5"
-                ><i class="fa-solid fa-arrow-up-right-from-square text-xs scale-[75%]" /></span></sup
+            >luisgrodriguezs<sup class=""><span class="text-xs scale-75 pl-0.5"></span></sup
           ></a>
         </p>
       </div>
