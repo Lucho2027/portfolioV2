908a26f2528b4a9405f9aac9d0355740b129da8a
Lucho2027 <luis01.ro@gmail.com>
2024-02-02 07:46:08 -0600

    chore: creating default layout to handle the looks on route change

--- layouts/Default.vue
+++ layouts/Default.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="container relative flex flex-col max-w-[1400px] mx-auto w-full text-sm sm:text-base min-h-screen">
     <Header />
-    <NuxtPage />
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 sm:py-14 p-4 h-[85vh]">
+      <div class="z-[10] flex flex-col lg:justify-center text-center lg:text-left lg:ml-20 gap-6 md:gap-8 lg:gap-10">
+        <slot />
+      </div>
+    </div>
+
     <Footer />
   </div>
   <NuxtParticles id="tsparticles" @load="onLoad" url="/particles.json"></NuxtParticles>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="grid grid-cols-1 lg:grid-cols-2 gap-10 sm:py-14 p-4 h-[85vh]">
-    <div class="z-[10] flex flex-col lg:justify-center text-center lg:text-left lg:ml-20 gap-6 md:gap-8 lg:gap-10">
+  <div>
+    <div>
       <h2 class="font-semibold font-lato text-4xl sm:text-5xl md:text-6xl">
         Hi! I'm <span class="font-montserrat text-primary-400">Luis</span><br />Full Stack
         <span class="font-montserrat text-primary-400">Developer</span>
